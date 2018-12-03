06862b584172c37e2c518140014daf6b08c02b6d
Tareq Hasan <tareq1988@gmail.com>
2018-12-03 16:55:43 +0600

    initial commit

---
+++ mailhog.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Plugin Name: Mailhog for WordPress
+ * Description: This plugin routes your emails to MailHog for development purpose.
+ * Plugin URI: https://tareq.co
+ * Author: Tareq Hasan
+ * Author URI: https://tareq.co
+ * Version: 1.0
+ * License: GPL2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * WP MailHog
+ */
+class WP_MailHog {
+
+    function __construct() {
+        $this->define_constants();
+        $this->init_phpmailer();
+    }
+
+    /**
+     * Define constants
+     *
+     * @return void
+     */
+    public function define_constants() {
+
+        if ( ! defined( 'WP_MAILHOG_HOST') ) {
+            define( 'WP_MAILHOG_HOST', '127.0.0.1' );
+        }
+
+        if ( ! defined( 'WP_MAILHOG_PORT') ) {
+            define( 'WP_MAILHOG_PORT', 1025 );
+        }
+    }
+
+    /**
+     * Override the PHPMailer SMTP options
+     *
+     * @return void
+     */
+    public function init_phpmailer() {
+        add_action( 'phpmailer_init', function( $phpmailer ) {
+            $phpmailer->Host     = WP_MAILHOG_HOST;
+            $phpmailer->Port     = WP_MAILHOG_PORT;
+            $phpmailer->SMTPAuth = false;
+            $phpmailer->isSMTP();
+        } );
+    }
+}
+
+new WP_MailHog();
---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== MailHog for WordPress ===
+Contributors: tareq1988
+Donate link: https://tareq.co/donate/
+Tags: smtp, mail, developer, email
+Requires at least: 4.0
+Tested up to: 4.8
+Stable tag: trunk
+Requires PHP: 5.3
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Zero configuration MailHog plugin for your development machine.
+
+== Description ==
+
+This plugin routes your emails to MailHog for development purpose.
+
+If you are using [MailHog](https://github.com/mailhog/MailHog) on your development environment, you can use this plugin to route your emails to MailHog.
+
+== Installation ==
+
+1. Upload the plugin folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= How can I use the plugin? =
+
+Just activate the plugin, you don't have to touch or change anything.
+
+= What is MailHog? =
+
+MailHog is an email testing tool for developers.
+
+
+== Screenshots ==
+
+
+== Changelog ==
+
+
+== Upgrade Notice ==
+
+Nothing here yet!
